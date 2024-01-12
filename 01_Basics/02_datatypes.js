261b184b8ae46ad19707b211af725e8240dc3082
Mohit Jain <mohit.sangita.jain@gmail.com>
2024-01-12 22:19:14 +0000

    data_types

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // all code is treated as newer version
+
+let myname = "Mohit" // string
+let age = 28 // int
+let married = false // boolean
+
+// for int range check google.
+// bigint is also available for sites related to stock market
+// boolean => true/false
+// null => standalone value (representation of empty value)
+// undefined => variable declared but not given any value
+// symbol => In react, used to find component is unique or not
+
+// object
+let mrvar
+console.log(typeof myvar)
+console.log(typeof null)
+// null is object
+// while undefined is undefined
