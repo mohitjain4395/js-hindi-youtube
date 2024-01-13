0cbaffe610c3aedd5a37662e43a5c7cb6b6e4fbb
Mohit Jain <mohit.sangita.jain@gmail.com>
2024-01-13 13:12:46 +0000

    operations done

---
+++ 01_Basics/03_dt_conversions.js
@@ -0,0 +1,46 @@
+// converting int to string
+/*
+let score = "33"
+let score_in_num = Number(score)
+
+console.log(score);
+console.log(score_in_num);
+
+console.log(typeof score);
+console.log(typeof score_in_num);
+
+// it converts string to number without any error even when it is not convertible, but its value is NaN (not a number)
+let num = "33abc"
+//let num = null
+//let num = undefined // in this case also, it convert to NaN
+//let num = true // it will 1(true) and 0(false)
+let num_in_int = Number(num)
+console.log(typeof num_in_int);
+console.log(num_in_int);
+
+// also, it converts null to 0
+*/
+// ---------------------------------------------------------------------------------------------
+// convert int to boolean
+//let login = 1
+// let login = "" => false
+// let login = "hello" => true
+//let login_bool = Boolean(login)
+//console.log(login_bool);
+// ---------------------------------------------------------------------------------------------
+// convert to string
+
+let num = 45
+let num_in_str = String(num)
+console.log(num_in_str);
+console.log(typeof num_in_str);
+// ---------------------------------------------------------------------------------------------
+// operations
+
+console.log(3+2);
+console.log(3-2);
+console.log(3*2);
+console.log(3/2);
+console.log(3**2);
+console.log(3%2);
+// ---------------------------------------------------------------------------------------------
